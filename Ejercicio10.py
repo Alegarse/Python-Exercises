7ad75c7125841760955fa335884f386a7144f7b4
Alejandro Garcia <agscm2@gmail.com>
2020-03-09 12:36:26 +0100

    Subida de Ejercicios

--- Ejercicio10.py
+++ Ejercicio10.py
@@ -1,3 +1,20 @@
 #! /usr/bin/python3
 
 # Ejercicio 10. Programación Orientada a Objetos
+
+from claseVehiculo import Vehiculo
+
+# Creo mi vehiculo
+
+mi3008 = Vehiculo("Peugeot3008")
+
+# Enumero las llamadas que se plantean en el Ejercicio
+
+print(mi3008.gasolina)
+mi3008.arrancar()
+mi3008.conducir()
+mi3008.conducir()
+mi3008.conducir()
+mi3008.conducir()
+mi3008.arrancar()
+print(mi3008.gasolina)
---
+++ claseVehiculo.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+
+# Ejercicio 10. Programación Orientada a Objetos
+
+class Vehiculo() :    
+    cantidad = 0
+    color = "Gris Amazonite"
+    ancho = 2.098
+    alto = 1.624
+    largo = 4.447
+    ruedas = 4
+    gasolina = 3
+
+    def __init__(self, nombre) :
+        self.nombre = nombre
+        Vehiculo.cantidad +=1
+    
+    def acelerar(self) :
+        print("Acelera")
+        Vehiculo.gasolina -= 1
+
+    def frenar(self) :
+        print("Frena")
+
+    def arrancar(self) :
+        if Vehiculo.gasolina >= 1 :
+            print("Arranca")
+            Vehiculo.gasolina -=1
+        else :
+            print("No arranca...")
+
+    def conducir(self) :
+        if Vehiculo.gasolina >=1 :
+            Vehiculo.gasolina -= 1
+            print("Quedan %d " % Vehiculo.gasolina)
+        else :
+            print("No se mueve...")
+
+    def repostar(self) :
+        Vehiculo.gasolina = 3
